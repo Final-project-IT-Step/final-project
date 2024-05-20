c83b7578728743201f27536e674638d596b32abc
Uliana Boiko <clubfate@gmail.com>
2024-05-20 20:11:02 +0300

    Tea Ceremony

---
+++ src/components/TeaCeremony/PrinciplesCeremony.jsx
@@ -0,0 +1,87 @@
+export const PrinciplesCeremony = () => {
+    return (
+        <div className="principles-tea-ceremony">
+            <h2 className="principles-tea-ceremony__title">
+                ЧОТИРИ ОСНОВНІ ПРИНЦИПИ ЧАЙНОЇ ЦЕРЕМОНІЇ
+            </h2>
+
+            <div className="principles-tea-ceremony__block">
+                <div className="principles-tea-ceremony__block_title">
+                    <div className="principles-tea-ceremony__block_title-box">
+                        <h2 className="text-green">ГАРМОНІЯ</h2>
+                        <p className="text-green text-bold text text-center">
+                            з собою, людьми, природою
+                        </p>
+                    </div>
+
+                    <div className="principles-tea-ceremony__block_title-box">
+                        <h2 className="text-green">ПОВАГА</h2>
+                        <p className="text-green text-bold text text-center">
+                            до всіх і всього
+                        </p>
+                    </div>
+
+                    <div className="principles-tea-ceremony__block_title-box">
+                        <h2 className="text-green">ЧИСТОТА</h2>
+                        <p className="text-green text-bold text text-center">
+                            фізична та духовна
+                        </p>
+                    </div>
+
+                    <div className="principles-tea-ceremony__block_title-box">
+                        <h2 className="text-green">СПОКІЙ</h2>
+                        <p className="text-green text-bold text text-center">
+                            в душі, відпочинок
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="principles-tea-ceremony__box-light">
+                <div className="light-box object-a">
+                    <div className="ball item">
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                    </div>
+                </div>
+
+                <div className="light-box object-b">
+                    <div className="ball item">
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                    </div>
+                </div>
+
+                <div className="light-box object-c">
+                    <div className="ball item">
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                    </div>
+                </div>
+
+                <div className="light-box object-d">
+                    <div className="ball item">
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
